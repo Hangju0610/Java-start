586743f05d1e1c9cdfb5bf00e40b9e5f218658b8
hangju0610 <phj950610@naver.com>
2023-12-14 19:47:01 +0900

    Feat: Add variable package 변수 연습 진행

---
+++ src/variable/Var4.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        // 변수는 여러 개를 한번에 선언할 수 있다.
+        int c,d,e,f;
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,12 @@
+package variable;
+
+import java.util.Date;
+
+public class Var6 {
+    public static void main(String[] args) {
+        // 변수를 초기화시키지 않으면 컴파일 에러
+        // variable a might not have been initialized
+        int a;
+//        System.out.println(a);
+    }
+}
