4c5d713f37a8d8de0b27765b2be85b5a82a6b00a
hangju0610 <phj950610@naver.com>
2023-12-14 15:09:40 +0900

    Init: 자바 연습

---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+//    프로그램 시작 시점
+    public static void main(String[] args) {
+        // 내부에 실행할 코드를 작성한다.
+        System.out.println("hello java");
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,10 @@
+public class HelloJava2 {
+    // 위에서 아래로 순차적으로 코드를 읽습니다.
+    // psvm => public static void main(String[] args)
+    // sout => system.out.println()
+    public static void main(String[] args) {
+        System.out.println("Hello java1");
+        System.out.println("Hello java2");
+        System.out.println("Hello java3");
+    }
+}
